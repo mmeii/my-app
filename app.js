4d40ff85f355b2272a71f88f3299e741b206d27d
mmeii <redmmeii@gmail.com>
2021-09-27 17:44:40 -0700

    add node/express server boiler plate

---
+++ app.js
@@ -0,0 +1,9 @@
+//express server
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+if(process.env.NODE_ENV === 'production') {
+  app.get('/*', function (req, res) {
+   	res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
+}
